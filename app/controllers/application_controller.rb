7fe546b818504ea186d087bacc602a6e5f89067a
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2025-07-15 18:44:41 +0000

    dynamic routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,43 +1,13 @@
 class ApplicationController < ActionController::Base
-  def home
+  def dynamic_dice
+    @num_dice = params[:dice].to_i
+    @sides = params[:sides].to_i
+    @rolls = Array.new(@num_dice) { rand(1..@sides) }
 
-      render({ :template => "application_templates/home"}) 
-end
-def two_six
-  @rolls = []
-2.times do
-  dice = rand(1..6)
-  @rolls.push(dice)
-end
-render({ :template => "application_templates/two_six"})
-end
-
-def two_ten
-  @rolls = []
-2.times do 
-  dice = rand(1..10)
-  @rolls.push(dice)
-end
-render({ :template => "application_templates/two_ten"})
-end
-
-def one_twenty
-    @rolls = []
-  1.times do 
-    dice = rand(1..20)
-    @rolls.push(dice)
+    render({ template: "application_templates/dynamic_dice" })
   end
-  render({ :template => "application_templates/one_twenty"})
-end
-
-def five_four
-  @rolls = []
-5.times do 
-  dice =rand(1..4)
-  @rolls.push(dice)
-end
-  render({ :template => "application_templates/five_four"})
-end
-
+  def home
+        render({ template: "application_templates/home" })
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
- 
-  get("/",{ :controller => "application", :action => "home"})
-  get("/dice/2/6",{ :controller => "application", :action => "two_six"})
-  get("/dice/2/10",{ :controller => "application", :action => "two_ten"})
-  get("/dice/1/20",{ :controller => "application", :action => "one_twenty"})
-  get("/dice/5/4",{ :controller => "application", :action => "five_four"})
+get("/dice/:dice/:sides", to: "application#dynamic_dice")
+get("/", to: "application#home")
+
+
 end
