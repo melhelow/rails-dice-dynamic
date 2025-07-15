145cc8b7baa7d974cdf1892ef7b321c9328891f9
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2025-07-15 17:51:16 +0000

    rcav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,43 @@
 class ApplicationController < ActionController::Base
-  skip_forgery_protection
+  def home
+
+      render({ :template => "application_templates/home"}) 
+end
+def two_six
+  @rolls = []
+2.times do
+  dice = rand(1..6)
+  @rolls.push(dice)
+end
+render({ :template => "application_templates/two_six"})
+end
+
+def two_ten
+  @rolls = []
+2.times do 
+  dice = rand(1..10)
+  @rolls.push(dice)
+end
+render({ :template => "application_templates/two_ten"})
+end
+
+def one_twenty
+    @rolls = []
+  1.times do 
+    dice = rand(1..20)
+    @rolls.push(dice)
+  end
+  render({ :template => "application_templates/one_twenty"})
+end
+
+def five_four
+  @rolls = []
+5.times do 
+  dice =rand(1..4)
+  @rolls.push(dice)
+end
+  render({ :template => "application_templates/five_four"})
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-
+ 
+  get("/",{ :controller => "application", :action => "home"})
+  get("/dice/2/6",{ :controller => "application", :action => "two_six"})
+  get("/dice/2/10",{ :controller => "application", :action => "two_ten"})
+  get("/dice/1/20",{ :controller => "application", :action => "one_twenty"})
+  get("/dice/5/4",{ :controller => "application", :action => "five_four"})
 end
